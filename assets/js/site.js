6d7cfe404c8a12f07173ec72fd08c9fc8c49b1bd
Bo Nicolajsen <>
2024-08-27 23:06:01 +0200

    commented.

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,63 +1,80 @@
 // write cool JS hwere!!
 let myData = [] // array der indeholder alle list descriptions
-let myCurrentList = null  // index of current list
+let myCurrentList = 0  // index of current list
 
+// element hvor min app skal leve
 const myAppElement = document.getElementById('app')
 
 
-makeDummyData()
-buildStatics()
-showList()
 
-function buildStatics() {
-
-    let myHeader = buildHeader()
-    let myFooter = buildFooter()
-
-    myAppElement.innerHTML = `${myHeader}<section id="content"></section>${myFooter}`
-
-}
+//Controller kode. aplication start funktion
+readMyData()
 
 
+// Controller kode. den spørger om data og handler udfra hvad den får tilbage.
+function readMyData() {
+    // kald ReadObject og sæt min data array til det der kommer tilbage
+    myData = ReadObject()
 
-function buildHeader() {
+    if (myData) {
+        // sætter den aktive liste til første liste i data
+        myCurrentList = 0
+    } else {
+        // hvis vi ikke har data 
+        console.log('we have no data');
+        myData = [] // opretter en tom data liste
+        myCurrentList = -1 // sætter current list til -1 som betyder ingen liste
+    }
 
-    // data
-    let myListData = myData[myCurrentList]
-
-    // dom element
-
-    let myHeader = `<header><h2>${myListData.name}</h2></header><ul>`
-
-
-
-    myData.forEach((listItem, index) => {
-
-
-        myHeader += `<li onclick="listCallBack(${index})">${listItem.name}</li>`
-    });
-
-    myHeader += `</ul>`
-
-
-
-
-    return myHeader
-
-}
-
-function listCallBack(indexClicked) {
-
-
-    myCurrentList = indexClicked
+    buildStatics()
     showList()
 }
 
 
-function buildFooter() {
 
-    let myHeader = `<footer>footer to complete</footer>`
-    return myHeader
+// View code. bygger statiske elementer
+function buildStatics() {
+    // header
+    let myHtml = `<header><img src="assets/img/list.svg" onclick="toggleList()"><h2 id="listTilte"></h2></header>`
+    // menu element
+    myHtml += `<section id="myMenu" class="hidden"></section>`
+    // content section
+    myHtml += `<section id="content"></section>`
+    // footer
+    myHtml += `<footer><img src="assets/img/list.svg" onclick="newListView()"><img src="assets/img/list.svg" onclick="removeListCallback()"></footer>`
+
+    myAppElement.innerHTML = myHtml
+    createListMenu()
+}
+
+
+// View code. bygger en liste med navnene på mine todo lists i data
+function createListMenu() {
+    let menuElement = document.getElementById('myMenu')
+
+    let myMenuHtml = '<ul>'
+
+    myData.forEach((listItem, index) => {
+        myMenuHtml += `<li onclick="listMenuCallBack(${index})">${listItem.name}</li>`
+    });
+
+    myMenuHtml += `</ul>`
+    menuElement.innerHTML = myMenuHtml
+
+}
+
+
+// Controller kode. callback til vis meny icon i header
+function toggleList() {
+    let menuElement = document.getElementById('myMenu')
+    menuElement.classList.toggle('hidden')
+}
+
+// Controller kode. callback til menu list. modtager index fra listen
+function listMenuCallBack(indexClicked) {
+    myCurrentList = indexClicked
+    toggleList()
+    showList()
 }
 
 
@@ -65,10 +82,125 @@ function buildFooter() {
 
 
 
+//View kode. viser den aktive liste i content
+function showList() {
+
+    // dom elementer
+    let myListElement = document.getElementById('content')
+    let menyTitleElement = document.getElementById('listTilte')
+
+    // data
+    let myListData = myData[myCurrentList]
 
 
-// modtager et navn,string og skaber et ny liste dataobjekt og gemmer det i myData-------------------------
+    let MyHtml = '' // variabel der skal fyldes med HTML kode
+
+    if (myListData) {
+        // vi har data til en liste
+        menyTitleElement.innerText = myListData.name
+
+        // tømmer dom elementet
+        myListElement.innerHTML = ''
+        MyHtml = `<ul>`
+
+        myListData.listItems.forEach((listItem, index) => {
+            MyHtml += `<li onclick="itemCallBack(${index})">${listItem.name}</li>`
+        });
+
+        MyHtml += `<li onclick="newItemView()">+</li>`
+        MyHtml += `</ul>`
+    } else {
+        // vi har ikke data til en liste
+        MyHtml = ''
+        menyTitleElement.innerText = 'lav en ny liste'
+    }
+
+    myListElement.innerHTML = MyHtml
+}
+
+// Controller kode. modtager index fra klik på list elementer
+function itemCallBack(indexClicked) {
+
+    if (confirm("Vil du slette mig?")) {
+        removeItem(myCurrentList, indexClicked)
+        showList()
+    }
+}
+
+// View kode. opretter view hvor man kan oprette ny liste
+function newListView() {
+    let myViewElement = document.getElementById('content')
+    myViewElement.innerHTML = ""
+
+    let myHtml = ` <input type="text" id="listName" name="listName"><button onclick="newListViewCallback('ok')">ok</button><button onclick="newListViewCallback('cancel')">cancel</button>`
+
+    myViewElement.innerHTML = myHtml
+}
+
+// controller kode. reagerer på ok i newListview
+function newListViewCallback(myresult) {
+    if (myresult == 'ok') {
+        let myName = document.getElementById('listName').value
+        makeList(myName)
+        createListMenu()
+
+    }
+    showList()
+
+}
+
+// View kode. opretter view hvor man kan oprette en ny task i en liste
+function newItemView() {
+    let myViewElement = document.getElementById('content')
+    myViewElement.innerHTML = ""
+
+    let myHtml = ` <input type="text" id="Name" name="listName"><button onclick="newItemViewCallback('ok')">ok</button><button onclick="newItemViewCallback('cancel')">cancel</button>`
+
+    myViewElement.innerHTML = myHtml
+
+
+}
+
+// controller kode. reagerer på ok i newItemView
+function newItemViewCallback(myresult) {
+    if (myresult == 'ok') {
+        let myName = document.getElementById('Name').value
+        console.log(myName);
+        createListMenu()
+        makeItem(myName)
+
+    }
+    showList()
+
+}
+
+// controller kode. reagerer på klik på delete icon i footer 
+function removeListCallback() {
+    removeList()
+    createListMenu()
+    showList()
+}
+
+
+//----------------------------------------------------------------------
+// Model kode. gemmer data tilsendt i local storage
+function SaveObject(toDoData) {
+    let mySerializedData = JSON.stringify(toDoData)//konverterer modtaget data til string
+    localStorage.setItem('savedData', mySerializedData)// gemmer i localStorage
+}
+
+
+
+function ReadObject() {
+    // læser data i local storage
+    let myfoundData = localStorage.getItem('savedData')
+    return JSON.parse(myfoundData) // konverterer det læste data til array og objekter og sender det tilbage til hvor funktionen er kaldt
+}
+
+//Model kode.  modtager et navn,string og skaber et ny liste dataobjekt og gemmer det i myData
 function makeList(myName) {
+    console.log('make list');
+
     let myList = {
         name: myName,//key value pair
         listItems: []
@@ -77,105 +209,48 @@ function makeList(myName) {
     myData.push(myList)
     myCurrentList = myData.length - 1
 
+    SaveObject(myData)
+
 }
 
-// --------------------------------------------------------
+//Model kode. fjerner den aktive liste fra data
+function removeList() {
+
+    if (myData.length > 0) {
+        // hvis der er lister at fjerne
+        myData.splice(myCurrentList, 1)
+        myCurrentList = myData.length - 1
+    } else {
+        //hvis der ikke er flere lister at fjerne
+        myCurrentList = -1
+    }
+    SaveObject(myData)
+
+}
 
 
-// modtager et navn og opretter list item i første to do list
+//Model kode. modtager et navn og opretter list item i første to do list
 
 function makeItem(myName) {
 
+    // data struktur for liste data
     let myListItem = {
         name: myName,
         status: true
     }
 
     myData[myCurrentList].listItems.push(myListItem)
+
+    SaveObject(myData)
 }
 
 
 
-// modtager et index for listen, og et index for item, og fjerner dette item fra listen.
+//Model kode. modtager et index for listen, og et index for item, og fjerner dette item fra listen.
 
 function removeItem(listIndex, itemIndex) {
-
     let myList = myData[listIndex]
-
-
-
     myList.listItems.splice(itemIndex, 1)
-
 }
 
 
-
-
-
-function showList() {
-
-    // data
-    let myListData = myData[myCurrentList]
-
-    // dom element
-    let myListElement = document.getElementById('content')
-
-    // tømmer dom elementet
-    myListElement.innerHTML = ''
-
-    // tekst variabel indeholdende html
-    let MyHtml = `<h2>${myListData.name}</h2><ul>`
-
-    myListData.listItems.forEach((listItem, index) => {
-
-
-        MyHtml += `<li onclick="itemCallBack(${index})">${listItem.name}</li>`
-    });
-
-    MyHtml += `<li onclick="itemCallBack('new')">+</li>`
-    MyHtml += `</ul>`
-
-    myListElement.innerHTML = MyHtml
-
-
-
-}
-
-function itemCallBack(indexClicked) {
-
-    if (indexClicked == "new") {
-
-        let newItem = prompt("tittel", "to do item");
-
-
-        if (newItem == null || newItem == "") {
-
-        } else {
-
-            makeItem(newItem)
-            showList()
-        }
-
-    } else {
-        if (confirm("delete task!")) {
-            removeItem(myCurrentList, indexClicked)
-            showList()
-        }
-
-    }
-
-
-}
-
-
-
-
-//----------------------------------------------------------------------
-
-function makeDummyData() {
-    makeList("liste 1")
-    makeList("liste 2")
-
-
-
-}
